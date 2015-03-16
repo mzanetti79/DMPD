150e015f67feef3a3433b6fd198a13e3aa0072b4
Alberto <a.zucchetta@cern.ch>
2015-03-16 12:27:11 +0100

    Add merger file

--- Heppy/python/analysis/merge.sh
+++ Heppy/python/analysis/merge.sh
@@ -5,7 +5,7 @@ filedir=/lustre/cmsdata/DM/ntuples/Prod_v01/weighted
 for s in DYJetsToLL GJets QCD WJetsToLNu ZJetsToNuNu
 do
   echo Merging $s...
-  hadd -f $s.root $filedir/$s\_*.root
+  hadd -f $filedir/$s.root $filedir/$s\_*.root
   #rm $sample_*.root
 done
 echo Done.
