245c28cb0760007916d4b5d06c1eada7513cf70a
Alberto <a.zucchetta@cern.ch>
2015-04-01 12:13:11 +0200

    Added splitTrees script

--- Heppy/python/analysis/merge.sh
+++ Heppy/python/analysis/merge.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 
-filedir=/lustre/cmsdata/DM/ntuples/Prod_v01/weighted
+filedir=/lustre/cmsdata/DM/ntuples/Prod_v02/weighted
 
-for s in DYJetsToLL GJets QCD WJetsToLNu ZJetsToNuNu
+for s in DYJetsToLL GJets QCD WJetsToLNu ZJetsToNuNu 
 do
   echo Merging $s...
-  hadd -f $filedir/$s.root $filedir/$s\_*.root
+  hadd -f $s.root $filedir/$s\_*.root
   #rm $sample_*.root
 done
+hadd -f ST.root $filedir/T\_*.root $filedir/Tbar\_*.root
+cp $filedir/TT.root .
 echo Done.
 
