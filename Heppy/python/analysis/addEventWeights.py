b36e9f5e872441cecccf77c77bbd335e1d2f5deb
Alberto <a.zucchetta@cern.ch>
2015-04-23 18:23:01 +0200

    Added histogram rescaling and recursive reading

--- Heppy/python/analysis/addEventWeights.py
+++ Heppy/python/analysis/addEventWeights.py
@@ -1,65 +1,66 @@
 #! /usr/bin/env python
 
-
 import os
 from array import array
-from ROOT import TFile, TH1D
+from ROOT import TFile, TH1
 
 from DMPD.Heppy.samples.Phys14.fileLists import samples
 
-prod_version = 'Prod_v01/'
-heppy_output_dir = '/lustre/cmsdata/DM/ntuples/'+prod_version
+heppy_output_dir = '/lustre/cmsdata/DM/ntuples/Prod_v01/'
 
 if not os.path.exists(heppy_output_dir+'weighted'):
-    print 'output dir does not exist, creating it'
+    print 'Output dir does not exist, creating it'
     os.makedirs(heppy_output_dir+'weighted')
 
-for ref_file_name in samples.keys(): 
-
-    print "##################################################"
-    print " Processing", ref_file_name.replace(heppy_output_dir,'')
-    print "##################################################"
+for ref_file_name in samples.keys():
+    #print "##################################################"
+    print "Processing:", ref_file_name.replace(heppy_output_dir, '')
+    #print "##################################################"
 
     # Unweighted input
     ref_file_name_with_path = heppy_output_dir+ref_file_name+'/tree.root'
     if not os.path.exists(ref_file_name_with_path): 
-        print 'WARNING:', ref_file_name_with_path, 'does not exist, continuing' 
+        print "WARNING:", ref_file_name_with_path, "does not exist, continuing"
         continue
-    ref_file = TFile(ref_file_name_with_path)
-    hCounters = ref_file.Get('Counter') 
-    totalEntries = hCounters.GetBinContent(1)
-
+    
     # Weighted output
-    new_file_name_with_path = heppy_output_dir+'weighted/'+ref_file_name+'.root'
+    new_file_name_with_path = heppy_output_dir+"weighted/"+ref_file_name+".root"
     if os.path.exists(new_file_name_with_path):
-        print new_file_name_with_path, 'exists, continuing' 
+        print "WARNING:", new_file_name_with_path, 'exists, continuing' 
         continue
-    new_file = TFile(new_file_name_with_path,'recreate')
-
-    # looping over trees
-    for tree_name in ['SR','ZCR','WCR','GCR']: 
-        print tree_name
-        tree = ref_file.Get(tree_name)
-        newTree = tree.CloneTree(-1,'fast')
-
-        # weight
-        weight = array('f',[1.0]) 
-        weightBranch = newTree.Branch('weight',weight,'weight/F') 
-
-        # looping over events
-        for event in range(0, tree.GetEntries()):
-            if event%100000==0: print event 
-            tree.GetEntry(event)
-            # set the weight somehow
-            #weight[0] = tree.xsec*tree.puWeight*muWeight[0]
-            weight[0] = samples[ref_file_name]['xsec']/totalEntries 
-            # fill the branch
-            weightBranch.Fill()
-
-        new_file.cd()
-        newTree.Write()
-
-    new_file.Close()
-
-
+    new_file = TFile(new_file_name_with_path, "RECREATE")
+    new_file.cd()
+    
+    # Get event number
+    ref_file = TFile(ref_file_name_with_path, "READ")
+    ref_hist = ref_file.Get('Counter')
+    totalEntries = ref_hist.GetBinContent(1)
+    weightXS = samples[ref_file_name]['xsec']/totalEntries 
+    
+    # Looping over file content
+    for key in ref_file.GetListOfKeys():
+        obj = key.ReadObj()
+        # Copy and rescale histograms
+        if obj.IsA().InheritsFrom("TH1"):
+            print "  TH1:", obj.GetName()
+            obj.Scale()
+            new_file.cd(weightXS)
+            obj.Write()
+        # Copy trees
+        elif obj.IsA().InheritsFrom("TTree"):
+            print "  TTree:", obj.GetName()
+            new_tree = obj.CloneTree(-1, 'fast')
+            weight = array('f',[1.0])  # weight
+            weightBranch = new_tree.Branch('weight',weight,'weight/F') 
+            # looping over events
+            for event in range(0, obj.GetEntries()):
+                #if event%100000==0: print event 
+                obj.GetEntry(event)
+                weight[0] = weightXS
+                weightBranch.Fill() # fill the branch
+            new_file.cd()
+            new_tree.Write()
+        else:
+            print "  Unknown object:", obj.GetName()
+    new_file.Close() 
 
