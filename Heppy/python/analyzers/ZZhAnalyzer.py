75b052f20e91add7836ce09087782a5fc4ac5a54
Alberto <a.zucchetta@cern.ch>
2015-06-03 17:56:27 +0200

    Re-enabled AK8 jets

--- Heppy/python/analyzers/ZZhAnalyzer.py
+++ Heppy/python/analyzers/ZZhAnalyzer.py
@@ -33,7 +33,7 @@ class ZZhAnalyzer( Analyzer ):
         if not hasattr(event, "Z"):
             return True
         self.ZZhCounter.Fill(2) # Z cand
-        if len(event.cleanFatJets)<1 or event.cleanFatJets[0].pt() < self.cfg_ana.fatjet_pt:
+        if len(event.cleanJetsAK8)<1 or event.cleanJetsAK8[0].pt() < self.cfg_ana.fatjet_pt:
             return True
         self.ZZhCounter.Fill(3) # Jet pT
         if event.Z.pt() < self.cfg_ana.Z_pt:
@@ -45,15 +45,15 @@ class ZZhAnalyzer( Analyzer ):
         
         # Build candidates
         # Higgs candidate
-        #prunedJet = copy.deepcopy(event.cleanFatJets[0]) # Copy fatJet...
+        #prunedJet = copy.deepcopy(event.cleanJetsAK8[0]) # Copy fatJet...
         #prunedJet.setMass(prunedJet.userFloat("ak8PFJetsCHSPrunedLinks")) # ... and set the mass to the pruned mass
-        theH = event.cleanFatJets[0].p4()
-        theH.charge = event.cleanFatJets[0].charge()
-        theH.deltaR = deltaR(event.cleanFatJets[0].subJet1.eta(), event.cleanFatJets[0].subJet1.phi(), event.cleanFatJets[0].subJet2.eta(), event.cleanFatJets[0].subJet2.phi()) if hasattr(event.cleanFatJets[0], "subJet2") else -1.
-        theH.deltaEta = abs(event.cleanFatJets[0].subJet1.eta() - event.cleanFatJets[0].subJet2.eta()) if hasattr(event.cleanFatJets[0], "subJet2") else -9.
-        theH.deltaPhi = deltaPhi(event.cleanFatJets[0].subJet1.phi(), event.cleanFatJets[0].subJet2.phi()) if hasattr(event.cleanFatJets[0], "subJet2") else -9.
+        theH = event.cleanJetsAK8[0].p4()
+        theH.charge = event.cleanJetsAK8[0].charge()
+        theH.deltaR = deltaR(event.cleanJetsAK8[0].subJet1.eta(), event.cleanJetsAK8[0].subJet1.phi(), event.cleanJetsAK8[0].subJet2.eta(), event.cleanJetsAK8[0].subJet2.phi()) if hasattr(event.cleanJetsAK8[0], "subJet2") else -1.
+        theH.deltaEta = abs(event.cleanJetsAK8[0].subJet1.eta() - event.cleanJetsAK8[0].subJet2.eta()) if hasattr(event.cleanJetsAK8[0], "subJet2") else -9.
+        theH.deltaPhi = deltaPhi(event.cleanJetsAK8[0].subJet1.phi(), event.cleanJetsAK8[0].subJet2.phi()) if hasattr(event.cleanJetsAK8[0], "subJet2") else -9.
         theH.deltaPhi_met = deltaPhi(theH.phi(), event.met.phi())
-        theH.deltaPhi_jet1 = deltaPhi(theH.phi(), event.cleanFatJets[0].phi())
+        theH.deltaPhi_jet1 = deltaPhi(theH.phi(), event.cleanJetsAK8[0].phi())
         event.h = theH
         
         # Zprime candidate
@@ -66,9 +66,9 @@ class ZZhAnalyzer( Analyzer ):
         theA.deltaPhi_jet1 = deltaPhi(theA.phi(), event.h.phi())
         event.A = theA
         
-#        for j in event.cleanFatJets:
+#        for j in event.cleanJetsAK8:
 #            j.deltaPhi_met = deltaPhi(j.phi(), event.met.phi())
-#            j.deltaPhi_jet1 = deltaPhi(j.phi(), event.cleanFatJets[0].phi())
+#            j.deltaPhi_jet1 = deltaPhi(j.phi(), event.cleanJetsAK8[0].phi())
         
         
         
@@ -76,14 +76,14 @@ class ZZhAnalyzer( Analyzer ):
         # Estimate cuts
         if event.Z.mass() > self.cfg_ana.Zmass_low and event.Z.mass() < self.cfg_ana.Zmass_high:
             self.ZZhCounter.Fill(5) # Z mass
-            if event.cleanFatJets[0].userFloat("ak8PFJetsCHSPrunedLinks") > self.cfg_ana.fatJetMass_low and event.cleanFatJets[0].userFloat("ak8PFJetsCHSPrunedLinks") < self.cfg_ana.fatJetMass_high:
+            if event.cleanJetsAK8[0].userFloat("ak8PFJetsCHSPrunedLinks") > self.cfg_ana.fatJetMass_low and event.cleanJetsAK8[0].userFloat("ak8PFJetsCHSPrunedLinks") < self.cfg_ana.fatJetMass_high:
                 self.ZZhCounter.Fill(6) # h mass
                 if event.met.pt() < self.cfg_ana.met_pt:
                     self.ZZhCounter.Fill(7) # MET
-                    #if event.cleanFatJets[0].btag('combinedInclusiveSecondaryVertexV2BJetTags') > self.cfg_ana.fatJet_btag:
-                    if event.cleanFatJets[0].nSubJets >= 1 and event.cleanFatJets[0].subJet1.btag('combinedInclusiveSecondaryVertexV2BJetTags') > self.cfg_ana.fatJet_btag_1:
+                    #if event.cleanJetsAK8[0].btag('combinedInclusiveSecondaryVertexV2BJetTags') > self.cfg_ana.fatJet_btag:
+                    if event.cleanJetsAK8[0].nSubJets >= 1 and event.cleanJetsAK8[0].subJet1.btag('combinedInclusiveSecondaryVertexV2BJetTags') > self.cfg_ana.fatJet_btag_1:
                         self.ZZhCounter.Fill(8) # b-Jet1
-                        if event.cleanFatJets[0].nSubJets >= 2 and event.cleanFatJets[0].subJet2.btag('combinedInclusiveSecondaryVertexV2BJetTags') > self.cfg_ana.fatJet_btag_2: 
+                        if event.cleanJetsAK8[0].nSubJets >= 2 and event.cleanJetsAK8[0].subJet2.btag('combinedInclusiveSecondaryVertexV2BJetTags') > self.cfg_ana.fatJet_btag_2: 
                             self.ZZhCounter.Fill(9) # b-Jet2
 
         return True
