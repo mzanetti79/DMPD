645f3efce703e8123cbcc622e33bbd1f1335196c
Alberto <a.zucchetta@cern.ch>
2015-07-24 12:58:11 +0200

    Modified SyncAnalyzer

--- Heppy/python/analyzers/SyncAnalyzer.py
+++ Heppy/python/analyzers/SyncAnalyzer.py
@@ -13,56 +13,59 @@ class SyncAnalyzer( Analyzer ):
         super(SyncAnalyzer,self).beginLoop(setup)
         if "outputfile" in setup.services:
             setup.services["outputfile"].file.cd()
-            self.Sync = ROOT.TH1F("Sync", "Sync", 8, 0, 8)
+            setup.services["outputfile"].file.mkdir("Sync")
+            setup.services["outputfile"].file.cd("Sync")
+            
+            SRLabels = ["All events", "Jet pT > 110", "dPhi(j1,j2)<2.5", "MET > 200", "Jets < 3", "Lepton veto", "Tau veto", "Photon veto"]
+            ZCRLabels = ["Trigger", "#Jets > 1", "Jet cuts", "Lep #geq 2", "Lep1 cuts", "Lep2 cuts", "Z cand", "MEt cut"]
+            WCRLabels = ["Trigger", "#Jets > 1", "Jet cuts", "Lep #geq 2", "Lep1 cuts", "Lep2 cuts", "Z cand", "MEt cut"]
+            
+            self.SR = ROOT.TH1F("SR", "Signal Region", 8, 0, 8)
+            self.ZCR = ROOT.TH1F("ZCR", "Z Control Region", 8, 0, 8)
+            self.WCR = ROOT.TH1F("WCR", "W Control Region", 8, 0, 8)
+            
+            for i, l in enumerate(SRLabels): self.SRCounter.GetXaxis().SetBinLabel(i+1, l)
+            for i, l in enumerate(ZCRLabels): self.ZCRCounter.GetXaxis().SetBinLabel(i+1, l)
+            for i, l in enumerate(WCRLabels): self.WCRCounter.GetXaxis().SetBinLabel(i+1, l)
+            
+    def syncSR(self, event):
     
-    def process(self, event):
-        self.Sync.GetXaxis().SetBinLabel(1, "All events")
         self.Sync.Fill(0)
         if len(event.cleanJets) >= 1 and event.cleanJets[0].pt()>110. and event.cleanJets[0].chargedHadronEnergyFraction()>0.2 and event.cleanJets[0].neutralHadronEnergyFraction()<0.7 and event.cleanJets[0].neutralEmEnergyFraction()<0.7:
-            self.Sync.GetXaxis().SetBinLabel(2, "Jet pT > 110")
-            self.Sync.Fill(1)
-            # print
-            #print "* %5d * %5d * %5d * %10.3f * %10.3f * %10.3f * %10.3f * %10.3f * %10.3f * %10.3f * %5d * %5d * %5d * %5d *" % ( event.input.eventAuxiliary().id().run(), event.input.eventAuxiliary().id().luminosityBlock(), event.input.eventAuxiliary().id().event(),  event.cleanJets[0].pt(), event.cleanJets[0].eta(), event.cleanJets[0].phi(), event.cleanJets[1].pt() if len(event.cleanJets)>1 else 0., event.cleanJets[1].eta() if len(event.cleanJets)>1 else 0., event.cleanJets[1].phi() if len(event.cleanJets)>1 else 0., event.met.pt(), len(event.cleanJets), len(event.inclusiveLeptons), len(event.selectedTaus), len(event.selectedPhotons))
-            
-            #if event.input.eventAuxiliary().id().event()==458: print "@@@@@@@@@@@@@@@@@@", abs(deltaPhi(event.cleanJets[0].phi(), event.cleanJets[1].phi())), event.cleanJets[1].neutralHadronEnergyFraction(), event.cleanJets[1].neutralEmEnergyFraction()
-            
-            #if (event.input.eventAuxiliary().id().luminosityBlock()==2 and event.input.eventAuxiliary().id().event()==136) or \
-               #(event.input.eventAuxiliary().id().luminosityBlock()==5 and event.input.eventAuxiliary().id().event()==475) or \
-               #(event.input.eventAuxiliary().id().luminosityBlock()==17 and event.input.eventAuxiliary().id().event()==1681) or \
-               #(event.input.eventAuxiliary().id().luminosityBlock()==64 and event.input.eventAuxiliary().id().event()==6397) or \
-               #(event.input.eventAuxiliary().id().luminosityBlock()==40 and event.input.eventAuxiliary().id().event()==3916) or \
-               #(event.input.eventAuxiliary().id().luminosityBlock()==56 and event.input.eventAuxiliary().id().event()==5579) or \
-               #(event.input.eventAuxiliary().id().luminosityBlock()==69 and event.input.eventAuxiliary().id().event()==6847) :
-                #print "* %5d * %5d * %5d * %10.3f * %10.3f * %10.3f * %10.3f * %10.3f * %10.3f * %10.3f * %5d * %5d * %5d * %5d *" % ( event.input.eventAuxiliary().id().run(), event.input.eventAuxiliary().id().luminosityBlock(), event.input.eventAuxiliary().id().event(),  event.cleanJets[0].pt(), event.cleanJets[0].eta(), event.cleanJets[0].phi(), event.cleanJets[1].pt() if len(event.cleanJets)>1 else 0., event.cleanJets[1].eta() if len(event.cleanJets)>1 else 0., event.cleanJets[1].phi() if len(event.cleanJets)>1 else 0., event.met.pt(), len(event.cleanJets), len(event.inclusiveLeptons), len(event.selectedTaus), len(event.selectedPhotons))
-                
-                #print event.inclusiveLeptons[0].pdgId()
-                #print 'pt\t',event.inclusiveLeptons[0].pt()
-                #print 'eta\t',event.inclusiveLeptons[0].eta()
-                #print 'phi\t',event.inclusiveLeptons[0].phi()
-                #print 'isLoose\t\t',event.inclusiveLeptons[0].isLooseMuon()
-                #print 'isGlobal\t',event.inclusiveLeptons[0].isGlobalMuon()
-                #print 'isTracker\t',event.inclusiveLeptons[0].isTrackerMuon()
-                                                            
+            self.Sync.Fill(1)                               
             if len(event.cleanJets) < 2 or ( abs(deltaPhi(event.cleanJets[0].phi(), event.cleanJets[1].phi())) < 2.5 and event.cleanJets[1].neutralHadronEnergyFraction()<0.7 and event.cleanJets[1].neutralEmEnergyFraction()<0.9 ):
-                self.Sync.GetXaxis().SetBinLabel(3, "dPhi(j1,j2)<2.5")
                 self.Sync.Fill(2)
                 if event.met.pt()>200.:
-                    self.Sync.GetXaxis().SetBinLabel(4, "MET > 200")
                     self.Sync.Fill(3)
                     if len(event.cleanJets)<3:
-                        self.Sync.GetXaxis().SetBinLabel(5, "Jets < 3")
                         self.Sync.Fill(4)
                         if len(event.inclusiveLeptons) == 0:
-                            self.Sync.GetXaxis().SetBinLabel(6, "Lepton veto")
                             self.Sync.Fill(5)
                             if len(event.selectedTaus) == 0:
-                                self.Sync.GetXaxis().SetBinLabel(7, "Tau veto")
                                 self.Sync.Fill(6)
                                 if len(event.selectedPhotons) == 0:
-                                    self.Sync.GetXaxis().SetBinLabel(8, "Photon veto")
                                     self.Sync.Fill(7)
                                     
-                                    print "%d:%d:%d" % ( event.input.eventAuxiliary().id().run(), event.input.eventAuxiliary().id().luminosityBlock(), event.input.eventAuxiliary().id().event() )
+                                    #print "%d:%d:%d" % ( event.input.eventAuxiliary().id().run(), event.input.eventAuxiliary().id().luminosityBlock(), event.input.eventAuxiliary().id().event() )
+    
+    
+    def syncZCR(self, event):
+    
+    
+    
+    def process(self, event):
+        if event.isSR:
+            self.syncSR(event)
+        elif event.isZCR:
+            self.syncZCR(event)
+        else event.isWCR:
+            self.syncWCR(event)
+        
+        
+        
+        
+        
+        
                                 
         
         return True
