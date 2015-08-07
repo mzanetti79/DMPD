75ff42da4b278781e8dd68b34bff30bece6e2c30
Alberto <a.zucchetta@cern.ch>
2015-08-07 18:04:44 +0200

    Summer cleanings

--- Heppy/python/analysis/cfg.py
+++
@@ -1,8 +0,0 @@
-from setup import Configuration
-cfg=Configuration()
-from observables import Observable
-
-cfg.parametersSet['region'] = 'SR'
-cfg.parametersSet['observable'] = 'met'
-#cfg.parametersSet['observable'] = Observable(variable='ZpT',formula='Z_pt',labelX='Z p_{T} [GeV]')
-cfg.parametersSet['selection'] = 'baseline'
