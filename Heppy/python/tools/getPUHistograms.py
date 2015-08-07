752014f68f63a9dc8906e91c28cf04a6787791b2
Alberto <a.zucchetta@cern.ch>
2015-08-07 18:17:20 +0200

    Added option for plots

--- Heppy/python/tools/getPUHistograms.py
+++ Heppy/python/tools/getPUHistograms.py
@@ -9,13 +9,15 @@ usage = "usage: %prog [options]"
 parser = optparse.OptionParser(usage)
 parser.add_option("-d", "--dataFile", action="store", type="string", default=False, dest="dataFileName")
 parser.add_option("-m", "--mcFile", action="store", type="string", default=False, dest="mcFileName")
+parser.add_option("-r", "--mcReweightedFile", action="store", type="string", default=False, dest="mcReweightedFileName")
 parser.add_option("-p", "--plot", action="store", default=False, dest="doPlot")
 parser.add_option("-s", "--save", action="store", default=True, dest="doSave")
 
 (options, args) = parser.parse_args()
 
 dataFileName = options.dataFile
-mcFileName = options.dataFile
+mcFileName = options.mcFile
+mcReweightedFileName = options.mcReweightedFileName
 doPlot = options.doPlot
 doSave = options.doSave
 
@@ -47,19 +49,19 @@ if doSave:
     print "Histograms written to ./PU.root file"
 
 if doPlot:
-    mc2File = TFile("/lustre/cmsdata/DM/ntuples/Prod_v08/weighted/DYJetsToLL_M50_amcatnloFXFX_pythia8_v3.root", "READ")
-    mc2Tree = mc2File.Get("ZCR")
-    mc2 = TH1F("mc", "nPV distribution", 60, 0, 60)
-    mc2.Sumw2()
-    mc2Tree.Project("mc", "nPV", "pileupWeight*abs(genWeight)/genWeight")
-    mc2.Scale(1./mc2.Integral())
-    mc2.SetLineWidth(2)
-    mc2.SetLineColor(2)
+    rmcFile = TFile(mcReweightedFileName, "READ")
+    rmcTree = rmcFile.Get("ZCR")
+    rmc = TH1F("mc", "nPV distribution", 60, 0, 60)
+    rmc.Sumw2()
+    rmcTree.Project("rmc", "nPV", "pileupWeight*abs(genWeight)/genWeight")
+    rmc.Scale(1./rmc.Integral())
+    rmc.SetLineWidth(2)
+    rmc.SetLineColor(2)
 
     c1 = TCanvas("c1", "PileUp reweighting", 800, 800)
     c1.cd()
     data.Draw("PE")
     mc.Draw("SAME, H")
-    mc2.Draw("SAME, H")
+    rmc.Draw("SAME, H")
     c1.Print("PU.pdf")
     c1.Print("PU.png")
