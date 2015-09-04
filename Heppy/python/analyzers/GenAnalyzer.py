c7b36ca690e1d6442e49f3e506e2d0ba2c5990b5
Alberto <a.zucchetta@cern.ch>
2015-09-04 18:37:01 +0200

    Migrated to Th1F dict and set Sumw2

--- Heppy/python/analyzers/GenAnalyzer.py
+++ Heppy/python/analyzers/GenAnalyzer.py
@@ -11,58 +11,59 @@ class GenAnalyzer( Analyzer ):
     
     def beginLoop(self,setup):
         super(GenAnalyzer,self).beginLoop(setup)
+        self.Hist = {}
         if "outputfile" in setup.services:
             setup.services["outputfile"].file.cd()
             setup.services["outputfile"].file.mkdir("Gen")
             setup.services["outputfile"].file.cd("Gen")
             
-            self.GenPhi1mass = ROOT.TH1F("GenPhi1mass", ";m_{X} [GeV]", 1000, 0., 5000.)
-            self.GenPhi1pt = ROOT.TH1F("GenPhi1pt", ";X p_{T} [GeV]", 500, 0., 1000.)
-            self.GenPhi1eta = ROOT.TH1F("GenPhi1eta", ";#Phi #eta", 50, -5, 5.)
-            self.GenChi1mass = ROOT.TH1F("GenChi1mass", ";#chi_{1} mass [GeV]", 1000, 0., 1000.)
-            self.GenChi1pt = ROOT.TH1F("GenChi1pt", ";#chi_{1} p_{T} [GeV]", 500, 0., 500.)
-            self.GenChi1eta = ROOT.TH1F("GenChi1eta", ";#chi_{1} #eta", 50, -5, 5.)
-            self.GenChi2mass = ROOT.TH1F("GenChi2mass", ";#chi_{2} mass [GeV]", 1000, 0., 100.)
-            self.GenChi2pt = ROOT.TH1F("GenChi2pt", ";#chi_{2} p_{T} [GeV]", 500, 0., 500.)
-            self.GenChi2eta = ROOT.TH1F("GenChi2eta", ";#chi_{2} #eta", 50, -5, 5.)
-            self.GenChi12dR = ROOT.TH1F("GenChi12dR", ";#chi_{1, 2} #Delta R", 50, 0, 5.)
+            self.Hist["GenPhi1mass"]"] = ROOT.TH1F("GenPhi1mass", ";m_{X} [GeV]", 1000, 0., 5000.)
+            self.Hist["GenPhi1pt"] = ROOT.TH1F("GenPhi1pt", ";X p_{T} [GeV]", 500, 0., 1000.)
+            self.Hist["GenPhi1eta"] = ROOT.TH1F("GenPhi1eta", ";#Phi #eta", 50, -5, 5.)
+            self.Hist["GenChi1mass"] = ROOT.TH1F("GenChi1mass", ";#chi_{1} mass [GeV]", 1000, 0., 1000.)
+            self.Hist["GenChi1pt"] = ROOT.TH1F("GenChi1pt", ";#chi_{1} p_{T} [GeV]", 500, 0., 500.)
+            self.Hist["GenChi1eta"] = ROOT.TH1F("GenChi1eta", ";#chi_{1} #eta", 50, -5, 5.)
+            self.Hist["GenChi2mass"] = ROOT.TH1F("GenChi2mass", ";#chi_{2} mass [GeV]", 1000, 0., 100.)
+            self.Hist["GenChi2pt"] = ROOT.TH1F("GenChi2pt", ";#chi_{2} p_{T} [GeV]", 500, 0., 500.)
+            self.Hist["GenChi2eta"] = ROOT.TH1F("GenChi2eta", ";#chi_{2} #eta", 50, -5, 5.)
+            self.Hist["GenChi12dR"] = ROOT.TH1F("GenChi12dR", ";#chi_{1, 2} #Delta R", 50, 0, 5.)
             
-            self.GenZdecay = ROOT.TH1F("GenZdecay", ";Z daughter pdgId", 25, 0.5, 25.5)
-            self.GenZmass = ROOT.TH1F("GenZmass", ";m_{Z} [GeV]", 150, 0., 150.)
-            self.GenZpt = ROOT.TH1F("GenZpt", ";Z p_{T} [GeV]", 250, 0., 2500.)
-            self.GenZeta = ROOT.TH1F("GenZeta", ";Z #eta", 50, -5, 5.)
-            self.GenZdR = ROOT.TH1F("GenZdR", ";Leptons #Delta R", 60, 0, 3.)
-            self.GenHdecay = ROOT.TH1F("GenHdecay", ";H daughter pdgId", 25, 0.5, 25.5)
-            self.GenHmass = ROOT.TH1F("GenHmass", ";m_{H} [GeV]", 100, 120., 130.)
-            self.GenHpt = ROOT.TH1F("GenHpt", ";H p_{T} [GeV]", 250, 0., 2500.)
-            self.GenHeta = ROOT.TH1F("GenHeta", ";H #eta", 50, -5, 5.)
-            self.GenHdR = ROOT.TH1F("GenHdR", ";b-quarks #Delta R", 50, 0, 5.)
-            self.GenHdPhi = ROOT.TH1F("GenHdPhi", ";b-quarks #Delta #varphi", 50, 0, 3.15)
-            self.GenLepton1pt = ROOT.TH1F("GenLepton1pt", ";Lepton 1 p_{T} [GeV]", 250, 0., 2500.)
-            self.GenLepton1eta = ROOT.TH1F("GenLepton1eta", ";Lepton 1 #eta", 50, -5, 5.)
-            self.GenLepton2pt = ROOT.TH1F("GenLepton2pt", ";Lepton 2 p_{T} [GeV]", 250, 0., 2500.)
-            self.GenLepton2eta = ROOT.TH1F("GenLepton2eta", ";Lepton 2 #eta", 50, -5, 5.)
-            self.GenBquark1pt = ROOT.TH1F("GenBquark1pt", ";b-quark 1 p_{T} [GeV]", 250, 0., 2500.)
-            self.GenBquark1eta = ROOT.TH1F("GenBquark1eta", ";b-quark 1 #eta", 50, -5, 5.)
-            self.GenBquark2pt = ROOT.TH1F("GenBquark2pt", ";b-quark 2 p_{T} [GeV]", 250, 0., 2500.)
-            self.GenBquark2eta = ROOT.TH1F("GenBquark2eta", ";b-quark 2 #eta", 50, -5, 5.)
-            #FIXME set Sumw2()
-            
+            self.Hist["GenZdecay"] = ROOT.TH1F("GenZdecay", ";Z daughter pdgId", 25, 0.5, 25.5)
+            self.Hist["GenZmass"] = ROOT.TH1F("GenZmass", ";m_{Z} [GeV]", 150, 0., 150.)
+            self.Hist["GenZpt"] = ROOT.TH1F("GenZpt", ";Z p_{T} [GeV]", 2500, 0., 2500.)
+            self.Hist["GenZeta"] = ROOT.TH1F("GenZeta", ";Z #eta", 50, -5, 5.)
+            self.Hist["GenZdR"] = ROOT.TH1F("GenZdR", ";Leptons #Delta R", 60, 0, 3.)
+            self.Hist["GenHdecay"] = ROOT.TH1F("GenHdecay", ";H daughter pdgId", 25, 0.5, 25.5)
+            self.Hist["GenHmass"] = ROOT.TH1F("GenHmass", ";m_{H} [GeV]", 100, 120., 130.)
+            self.Hist["GenHpt"] = ROOT.TH1F("GenHpt", ";H p_{T} [GeV]", 250, 0., 2500.)
+            self.Hist["GenHeta"] = ROOT.TH1F("GenHeta", ";H #eta", 50, -5, 5.)
+            self.Hist["GenHdR"] = ROOT.TH1F("GenHdR", ";b-quarks #Delta R", 50, 0, 5.)
+            self.Hist["GenHdPhi"] = ROOT.TH1F("GenHdPhi", ";b-quarks #Delta #varphi", 50, 0, 3.15)
+            self.Hist["GenLepton1pt"] = ROOT.TH1F("GenLepton1pt", ";Lepton 1 p_{T} [GeV]", 250, 0., 2500.)
+            self.Hist["GenLepton1eta"] = ROOT.TH1F("GenLepton1eta", ";Lepton 1 #eta", 50, -5, 5.)
+            self.Hist["GenLepton2pt"] = ROOT.TH1F("GenLepton2pt", ";Lepton 2 p_{T} [GeV]", 250, 0., 2500.)
+            self.Hist["GenLepton2eta"] = ROOT.TH1F("GenLepton2eta", ";Lepton 2 #eta", 50, -5, 5.)
+            self.Hist["GenBquark1pt"] = ROOT.TH1F("GenBquark1pt", ";b-quark 1 p_{T} [GeV]", 250, 0., 2500.)
+            self.Hist["GenBquark1eta"] = ROOT.TH1F("GenBquark1eta", ";b-quark 1 #eta", 50, -5, 5.)
+            self.Hist["GenBquark2pt"] = ROOT.TH1F("GenBquark2pt", ";b-quark 2 p_{T} [GeV]", 250, 0., 2500.)
+            self.Hist["GenBquark2eta"] = ROOT.TH1F("GenBquark2eta", ";b-quark 2 #eta", 50, -5, 5.)
             setup.services["outputfile"].file.cd("..")
             
-
+            # LHE
             setup.services["outputfile"].file.mkdir("Lhe")
             setup.services["outputfile"].file.cd("Lhe")
-            self.LheZpt = ROOT.TH1F("LheZpt", ";Z p_{T} [GeV]", 250, 0., 2500.)
-            self.LheHT = ROOT.TH1F("LheHT", ";HT [GeV]", 150, 0., 1500.)
-            self.LheNj = ROOT.TH1F("LheNj", ";Number of partons", 6, -0.5, 5.5)
-            self.LheNb = ROOT.TH1F("LheNb", ";Number of b-quarks", 6, -0.5, 5.5)
-            self.LheNc = ROOT.TH1F("LheNc", ";Number of c-quarks", 6, -0.5, 5.5)
-            self.LheNl = ROOT.TH1F("LheNl", ";Number of light quarks", 6, -0.5, 5.5)
-            self.LheNg = ROOT.TH1F("LheNg", ";Number of gluons", 6, -0.5, 5.5)
+            self.Hist["LheZpt"] = ROOT.TH1F("LheZpt", ";Z p_{T} [GeV]", 2500, 0., 2500.)
+            self.Hist["LheHT"] = ROOT.TH1F("LheHT", ";HT [GeV]", 150, 0., 1500.)
+            self.Hist["LheNj"] = ROOT.TH1F("LheNj", ";Number of partons", 6, -0.5, 5.5)
+            self.Hist["LheNb"] = ROOT.TH1F("LheNb", ";Number of b-quarks", 6, -0.5, 5.5)
+            self.Hist["LheNc"] = ROOT.TH1F("LheNc", ";Number of c-quarks", 6, -0.5, 5.5)
+            self.Hist["LheNl"] = ROOT.TH1F("LheNl", ";Number of light quarks", 6, -0.5, 5.5)
+            self.Hist["LheNg"] = ROOT.TH1F("LheNg", ";Number of gluons", 6, -0.5, 5.5)
             setup.services["outputfile"].file.cd("..")
             
-            
+            # Set Sumw2
+            for n, h in self.Hist.iteritems():
+                h.Sumw2()
     
     def process(self, event):
         
@@ -77,67 +78,67 @@ class GenAnalyzer( Analyzer ):
             if g.pdgId() in self.cfg_ana.chi:
                 event.genChi.append(g)
         if hasattr(event, "genPhi"):
-            self.GenPhi1mass.Fill(event.genPhi.mass())
-            self.GenPhi1pt.Fill(event.genPhi.pt())
-            self.GenPhi1eta.Fill(event.genPhi.eta())
+            self.Hist["GenPhi1mass"].Fill(event.genPhi.mass())
+            self.Hist["GenPhi1pt"].Fill(event.genPhi.pt())
+            self.Hist["GenPhi1eta"].Fill(event.genPhi.eta())
         if len(event.genChi) >= 2:
             i1, i2 = [0, 1] if event.genChi[0].pt() > event.genChi[1].pt() else [1, 0]
-            self.GenChi1mass.Fill(event.genChi[i1].mass())
-            self.GenChi1pt.Fill(event.genChi[i1].pt())
-            self.GenChi1eta.Fill(event.genChi[i1].eta())
-            self.GenChi2mass.Fill(event.genChi[i1].mass())
-            self.GenChi2pt.Fill(event.genChi[i1].pt())
-            self.GenChi2eta.Fill(event.genChi[i1].eta())
-            self.GenChi12dR.Fill(deltaR(event.genChi[0].eta(), event.genChi[0].phi(), event.genChi[1].eta(), event.genChi[1].phi()))
+            self.Hist["GenChi1mass"].Fill(event.genChi[i1].mass())
+            self.Hist["GenChi1pt"].Fill(event.genChi[i1].pt())
+            self.Hist["GenChi1eta"].Fill(event.genChi[i1].eta())
+            self.Hist["GenChi2mass"].Fill(event.genChi[i1].mass())
+            self.Hist["GenChi2pt"].Fill(event.genChi[i1].pt())
+            self.Hist["GenChi2eta"].Fill(event.genChi[i1].eta())
+            self.Hist["GenChi12dR"].Fill(deltaR(event.genChi[0].eta(), event.genChi[0].phi(), event.genChi[1].eta(), event.genChi[1].phi()))
         # Z
         if hasattr(event, "genVBosons"):
             if len(event.genVBosons) > 0:
-                self.GenZmass.Fill(event.genVBosons[0].mass())
-                self.GenZpt.Fill(event.genVBosons[0].pt())
-                self.GenZeta.Fill(event.genVBosons[0].eta())
+                self.Hist["GenZmass"].Fill(event.genVBosons[0].mass())
+                self.Hist["GenZpt"].Fill(event.genVBosons[0].pt())
+                self.Hist["GenZeta"].Fill(event.genVBosons[0].eta())
         # Higgs
         if hasattr(event, "genHiggsBosons"):
             if len(event.genHiggsBosons) > 0:
-                self.GenHmass.Fill(event.genHiggsBosons[0].mass())
-                self.GenHpt.Fill(event.genHiggsBosons[0].pt())
-                self.GenHeta.Fill(event.genHiggsBosons[0].eta())
+                self.Hist["GenHmass"].Fill(event.genHiggsBosons[0].mass())
+                self.Hist["GenHpt"].Fill(event.genHiggsBosons[0].pt())
+                self.Hist["GenHeta"].Fill(event.genHiggsBosons[0].eta())
         # Leptons from Z
         if hasattr(event, "genleps"):
             if len(event.genleps) >= 2:
-                self.GenZdecay.Fill(abs(event.genleps[0].pdgId()))
-                self.GenZdR.Fill(deltaR(event.genleps[0].eta(), event.genleps[0].phi(), event.genleps[1].eta(), event.genleps[1].phi()))
+                self.Hist["GenZdecay"].Fill(abs(event.genleps[0].pdgId()))
+                self.Hist["GenZdR"].Fill(deltaR(event.genleps[0].eta(), event.genleps[0].phi(), event.genleps[1].eta(), event.genleps[1].phi()))
                 i1, i2 = [0, 1] if event.genleps[0].pt() > event.genleps[1].pt() else [1, 0]
-                self.GenLepton1pt.Fill(event.genleps[i1].pt())
-                self.GenLepton1eta.Fill(event.genleps[i1].eta())
-                self.GenLepton2pt.Fill(event.genleps[i2].pt())
-                self.GenLepton2eta.Fill(event.genleps[i2].eta())
+                self.Hist["GenLepton1pt"].Fill(event.genleps[i1].pt())
+                self.Hist["GenLepton1eta"].Fill(event.genleps[i1].eta())
+                self.Hist["GenLepton2pt"].Fill(event.genleps[i2].pt())
+                self.Hist["GenLepton2eta"].Fill(event.genleps[i2].eta())
         # b-quarks from Higgs
         if hasattr(event, "genbquarks"):
             if len(event.genbquarks) == 1:
-                self.GenBquark1pt.Fill(event.genbquarks[0].pt())
-                self.GenBquark1eta.Fill(event.genbquarks[0].eta())
+                self.Hist["GenBquark1pt"].Fill(event.genbquarks[0].pt())
+                self.Hist["GenBquark1eta"].Fill(event.genbquarks[0].eta())
             elif len(event.genbquarks) >= 2:
-                self.GenHdecay.Fill(abs(event.genbquarks[0].pdgId()))
-                self.GenHdR.Fill(deltaR(event.genbquarks[0].eta(), event.genbquarks[0].phi(), event.genbquarks[1].eta(), event.genbquarks[1].phi()))
-                self.GenHdPhi.Fill(deltaPhi(event.genbquarks[0].phi(), event.genbquarks[1].phi()))
+                self.Hist["GenHdecay"].Fill(abs(event.genbquarks[0].pdgId()))
+                self.Hist["GenHdR"].Fill(deltaR(event.genbquarks[0].eta(), event.genbquarks[0].phi(), event.genbquarks[1].eta(), event.genbquarks[1].phi()))
+                self.Hist["GenHdPhi"].Fill(deltaPhi(event.genbquarks[0].phi(), event.genbquarks[1].phi()))
                 i1, i2 = [0, 1] if event.genbquarks[0].pt() > event.genbquarks[1].pt() else [1, 0]
-                self.GenBquark1pt.Fill(event.genbquarks[i1].pt())
-                self.GenBquark1eta.Fill(event.genbquarks[i1].eta())
-                self.GenBquark2pt.Fill(event.genbquarks[i2].pt())
-                self.GenBquark2eta.Fill(event.genbquarks[i2].eta())
+                self.Hist["GenBquark1pt"].Fill(event.genbquarks[i1].pt())
+                self.Hist["GenBquark1eta"].Fill(event.genbquarks[i1].eta())
+                self.Hist["GenBquark2pt"].Fill(event.genbquarks[i2].pt())
+                self.Hist["GenBquark2eta"].Fill(event.genbquarks[i2].eta())
         
         
         ### LHE event ###
         if not hasattr(event, "lheV_pt"):
             return True
             
-        self.LheZpt.Fill(event.lheV_pt)
-        self.LheHT.Fill(event.lheHT)
-        self.LheNj.Fill(event.lheNj)
-        self.LheNb.Fill(event.lheNb)
-        self.LheNc.Fill(event.lheNc)
-        self.LheNl.Fill(event.lheNl)
-        self.LheNg.Fill(event.lheNg)
+        self.Hist["LheZpt"].Fill(event.lheV_pt)
+        self.Hist["LheHT"].Fill(event.lheHT)
+        self.Hist["LheNj"].Fill(event.lheNj)
+        self.Hist["LheNb"].Fill(event.lheNb)
+        self.Hist["LheNc"].Fill(event.lheNc)
+        self.Hist["LheNl"].Fill(event.lheNl)
+        self.Hist["LheNg"].Fill(event.lheNg)
         
         
         return True
