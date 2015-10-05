85444674591a16cf36b46a47564c106d53f74749
Alberto <a.zucchetta@cern.ch>
2015-10-05 19:08:47 +0200

    Finally fixed newline in lsf output

--- Heppy/test/heppy_clean.sh
+++ Heppy/test/heppy_clean.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# | sed 's/ //g' | grep Batch | sed ':a;N;$!ba;s/\n/ /g' | sed 's/;.*//' | sed 's/t\///' | awk '{ print $1 }'
+
 for i in `bjobs -d | grep EXIT  | awk '{ print $1 }'`
 do
-    text=`bjobs -l $i | grep Chunk | sed ':a;N;$!ba;s/\n/ /g'  | awk '{ print $1 }'`
+    text=`bjobs -l $i  | sed ':a;N;$!ba;s/\n//g'  | sed 's/ //g' | sed 's/;/\n/g' | grep cd/ | sed 's/cd\//\//g'`
     echo $i $text
 done
 
